c5e94ab18d7cfec1ea8c6d1a5ccc4507e398e638
kieuminhtuan2112003@gmail.com <kieuminhtuan2112003@gmail.com>
2025-01-08 22:14:20 +0700

    Trang chu admin

--- socialnetwork2024/social/admin.py
+++ socialnetwork2024/social/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from social.models import Category
+from social.models import Category, Topic, Post
 
 admin.site.register(Category)
+admin.site.register(Topic)
+admin.site.register(Post)
--- socialnetwork2024/social/models.py
+++ socialnetwork2024/social/models.py
@@ -9,3 +9,34 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class BaseModel(models.Model):
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-id"]
+
+
+class Topic(BaseModel):
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField()
+    #vì Topic mình chỉ cần cái name với description thôi nên tui không thêm thuộc tính image ở trong này
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ['name', 'category']
+
+class Post(BaseModel):
+    title = models.CharField(max_length=255, null=False)
+    content = models.TextField(null=False)
+    image = models.ImageField(upload_to="posts/%Y/%m")
+    topic = models.ForeignKey(Topic, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.title

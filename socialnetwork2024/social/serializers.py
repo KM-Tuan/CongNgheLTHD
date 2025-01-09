c204ab7626f7a26a71d55cb96370c0421ba7b62b
kieuminhtuan2112003@gmail.com <kieuminhtuan2112003@gmail.com>
2025-01-09 10:48:44 +0700

    swagger quan ly cac APIs

--- socialnetwork2024/social/serializers.py
+++ socialnetwork2024/social/serializers.py
@@ -9,4 +9,16 @@ class CategorySerializer(serializers.ModelSerializer):
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'category_id']
+
+class PostSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField(source='image')
+
+    def get_image(self, post):
+        request =  self.context.get('request')
+        if request and post.image:
+            return request.build_absolute_uri('/static/%s' % post.image)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'image', 'topic_id']
--- socialnetwork2024/social/views.py
+++ socialnetwork2024/social/views.py
@@ -1,23 +1,34 @@
 from rest_framework import viewsets, generics
 from social.models import Category, Topic, Post
 from social import serializers, paginators
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
-class CategoryViewSet(viewsets.ViewSet, generics.ListAPIView): # API Category
+class CategoryViewSet(viewsets.ViewSet, generics.ListAPIView):  # API Category
     queryset = Category.objects.all()
     serializer_class = serializers.CategorySerializer
 
 
-class TopicViewSet(viewsets.ViewSet, generics.ListAPIView): # API Topic
+class TopicViewSet(viewsets.ViewSet, generics.ListAPIView):  # API Topic
     queryset = Topic.objects.filter(active=True)
     serializer_class = serializers.TopicSerializer
     pagination_class = paginators.TopicPagination
 
     def get_queryset(self):
         query = self.queryset
+        cate_id = self.request.query_params.get('category_id')
+
+        if cate_id:
+            query = query.filter(category_id=cate_id)  # Lấy danh sách các chủ đề thuộc danh mục
 
         q = self.request.query_params.get("q")
         if q:
-            query = query.filter(name__icontains=q)
+            query = query.filter(name__icontains=q)  # Tìm kiếm chủ đề theo tên
 
         return query
+
+    @action(methods=['get'], url_path='posts', detail=True) # Lấy danh sách các bài đăng thuộc chủ đề
+    def get_posts(self, request, pk):
+        posts = self.get_object().post_set.filter(active=True)
+        return Response(serializers.PostSerializer(posts, many=True, context={'request': request}).data)

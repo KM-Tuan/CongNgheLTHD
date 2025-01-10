3f47e1ae90004542a49e041fb42c18726ed3b5f1
TruongDucHoang <tantiennguyen2404@gmail.com>
2025-01-10 10:10:08 +0700

    change code

--- socialnetwork2024/social/serializers.py
+++ socialnetwork2024/social/serializers.py
@@ -53,7 +53,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'last_name', 'avatar', 'username', 'password']
+        fields = ['id', 'first_name', 'last_name', 'avatar', 'username', 'password']
         extra_kwargs = {
             'password': {
                 'write_only': True

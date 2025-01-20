0afcc55ef865939f0207c4cbccb31cdb2635d7a9
tdhoang159 <tantiennguyen2404@gmail.com>
2025-01-20 14:58:42 +0700

    Deno show categories

--- socialnetwork2024mobile/components/Home/Home.js
+++ socialnetwork2024mobile/components/Home/Home.js
@@ -1,12 +1,16 @@
 import { Text, View } from "react-native";
 import Styles from "../../styles/Styles";
 import { useEffect, useState } from "react";
+import APIs, { endpoints } from "../../configs/APIs";
+import { Chip } from "react-native-paper";
 
 const Home = () => {
     const [categories, setCategories] = useState([]);
 
     const loadCates = async () => {
-
+        let res = await APIs.get(endpoints['categories']);
+        console.info(res.data);
+        setCategories(res.data);
     }
 
     useEffect(() => {
@@ -16,6 +20,7 @@ const Home = () => {
     return (
         <View style={Styles.container}>
             <Text style={Styles.subject}>DANH MỤC KHÓA HỌC</Text>
+            {categories.map(c => <Chip icon="label" key={c.id}>{c.name}</Chip>)}
         </View>
     );
 }
--- socialnetwork2024mobile/configs/APIs.js
+++ socialnetwork2024mobile/configs/APIs.js
@@ -1,1 +1,11 @@
-const BASE_URL = ''
+import axios from "axios"
+
+const BASE_URL = 'https://thanhduong.pythonanywhere.com/'
+
+export const endpoints = {
+    'categories': '/categories/'
+}
+
+export default axios.create({
+    baseURL: BASE_URL
+});
